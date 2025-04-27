d22151fa1975b230226b4371d51040bbd0ad51a0
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-04-27 02:41:57 -0300

    Primer commit Ejercicio2 TextBox y Ddl

---
+++ TP4-Grupo18-Porgramcion/Ejercicio2.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio2.aspx.cs" Inherits="TP4_Grupo18_Porgramcion.Ejercicio2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            IdProducto:&nbsp;
+            <asp:DropDownList ID="ddlProducto" runat="server">
+            </asp:DropDownList>
+&nbsp;
+            <asp:TextBox ID="txtProducto" runat="server"></asp:TextBox>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ TP4-Grupo18-Porgramcion/Ejercicio2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP4_Grupo18_Porgramcion
+{
+    public partial class Ejercicio2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                //cargar opciones en los dropdownlist
+                ddlProducto.Items.Add("--Seleccionar--");
+                ddlProducto.Items.Add("Igual a");
+                ddlProducto.Items.Add("Mayor a");
+                ddlProducto.Items.Add("Menor a");
+
+            }
+
+        }
+    }
+}
