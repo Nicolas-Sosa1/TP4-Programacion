82492f0fe77ec1312cbbd930a43ac6bdc4391747
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-04-27 01:54:37 -0300

    PostBack Primer Ejercicio

--- TP4-Grupo18-Porgramcion/Ejercicio1.aspx.cs
+++ TP4-Grupo18-Porgramcion/Ejercicio1.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,19 @@ namespace TP4_Grupo18_Porgramcion
 {
     public partial class Ejercicio1 : System.Web.UI.Page
     {
+        private const string cadenaConexion = "Data Source=localhost\\sqlexpress;InitialCatalog=Viajes;Integrated Security = True";
+
+        //Variables para concatenar con consultas condicionales
+        string idProvincia;
+        string idProvinciaFin;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack == false)
+            {
+                SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
+                sqlConnection.Open();
 
+            }
         }
     }
 }
