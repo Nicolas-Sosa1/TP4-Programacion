1a34af219ed2d0011bf330f716709dce0456b87d
Alanjaqs <alannjaqs@gmail.com>
2025-04-28 01:45:25 -0300

    Aporte 3 ejercicio 3

--- TP4-Grupo18-Porgramcion/Ejercicio3.aspx.cs
+++ TP4-Grupo18-Porgramcion/Ejercicio3.aspx.cs
@@ -9,9 +9,14 @@ namespace TP4_Grupo18_Porgramcion
 {
     public partial class Ejercicio3 : System.Web.UI.Page
     {
+        private const string cadenaConexion = "Data Source=localhost\\sqlexpress;Initial Catalog=Libreria;Integrated Security=True";
+        private string consultaTemas = "SELECT * FROM Temas";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
 
+            }
         }
     }
 }
